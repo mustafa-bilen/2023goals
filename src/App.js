22219ae644700cc1a7b06c4567b370631d827b17
mustafa-bilen <mustafabilen6669@gmail.com>
2023-01-01 02:23:29 +0300

    thn

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <li>Moving to a livable country for permanent</li>
         <li>Commit to github every day</li>
         <li>I want to focus on myself</li>
+        <li>Stay away from depression and mental breakdowns</li>
+        <li>Getting my best friend into start to the sport</li>
         <li>Making an aesthetic body like "ZYZZ"</li>
       </ul>
     </div>
