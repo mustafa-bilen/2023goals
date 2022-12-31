8d207e8f2e9151e34e7a5a4be722ca40f0dc3aec
mustafa-bilen <mustafabilen6669@gmail.com>
2023-01-01 02:02:23 +0300

    updt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 function App() {
   return (
     <div className="bbg">
-      <div className="text-4xl text-lime-600 mb-5">My goals in 2023</div>
-      <ul className="text-2xl list-disc">
+      <div className="text-4xl text-lime-600">My goals in 2023</div>
+      <ul className="text-xl list-disc text-left">
         <li>Being happy</li>
         <li>Being a good human for world</li>
         <li>Get a job at a visionary company</li>
         <li>Moving to a livable country for permanent</li>
         <li>Commit to github every day</li>
         <li>I want to focus on myself</li>
+        <li>Making an aesthetic body like "ZYZZ"</li>
       </ul>
     </div>
   );
